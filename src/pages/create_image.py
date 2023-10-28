e7b60164f48804955db0c11e0b496773b538c66e
mori-takuya <58283134+mori-takuya@users.noreply.github.com>
2023-10-28 21:38:46 +0900

    Update create_image.py

--- src/pages/create_image.py
+++ src/pages/create_image.py
@@ -2,24 +2,18 @@ import openai
 import streamlit as st
 
 # OpenAI APIの設定
-openai.api_key = 'YOUR_API_KEY'
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # 画像を生成するリクエストの送信
-response = openai.Completion.create(
-  engine="davinci",
+response = openai.Image.create(
   prompt="a",
-  max_tokens=100,
-  temperature=0.7
+  n=1,
+  size='{}x{}'.format(str(50), str(50))
 )
 
 # 生成された画像のURLを取得
-image_url = response.choices[0].text.strip()
+image_url = response['data'][0]['url']
 
-# 画像を表示するなどの処理
-# 例えば、matplotlibライブラリを使用して画像を表示する場合
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-img = mpimg.imread(image_url)
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
-st.image(img, caption='画像キャプション')
+# 画像を表示する処理
+# user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+st.image(image_url, caption='画像キャプション')
