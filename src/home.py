2e7c33e94783a971fe8a4d3407b6318e28079dde
mori-takuya <58283134+mori-takuya@users.noreply.github.com>
2023-10-28 18:42:54 +0900

    Update home.py

--- src/home.py
+++ src/home.py
@@ -1,15 +1,2 @@
 # Contents of ~/my_app/streamlit_app.py
 import streamlit as st
-
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
-
-def openai_api_bot():
-    st.markdown("# openai_api_bot ❄️")
-    st.sidebar.markdown("# openai_api_bot ❄️")
-
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "openai_api_bot": openai_api_bot
-}
