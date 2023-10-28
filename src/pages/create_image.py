2f91f517bce2889fc1feeb2d3142c679fc96ae7f
mori-takuya <58283134+mori-takuya@users.noreply.github.com>
2023-10-28 21:46:25 +0900

    Update create_image.py

--- src/pages/create_image.py
+++ src/pages/create_image.py
@@ -5,15 +5,17 @@ import streamlit as st
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # 画像を生成するリクエストの送信
-response = openai.Image.create(
-  prompt= user_input,
-  n=1,
-  size='{}x{}'.format(str(256), str(256))
-)
-
-# 生成された画像のURLを取得
-image_url = response['data'][0]['url']
+def generate_image_with_dalle2(prompt):
+  response = openai.Image.create(
+    prompt= prompt,
+    n=1,
+    size='{}x{}'.format(str(256), str(256))
+  )
+  # 生成された画像のURLを取得
+  image_url = response['data'][0]['url']
+  return image_url
 
 # 画像を表示する処理
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+image_url = generate_image_with_dalle2(""" user_input """)
 st.image(image_url, caption='画像キャプション')
