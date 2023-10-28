10b68a22497598d575f58bba0db1eb0c4cd3ee8b
mori-takuya <58283134+mori-takuya@users.noreply.github.com>
2023-10-28 18:39:07 +0900

    Update home.py

--- src/home.py
+++ src/home.py
@@ -13,6 +13,3 @@ page_names_to_funcs = {
     "Main Page": main_page,
     "app": app
 }
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
