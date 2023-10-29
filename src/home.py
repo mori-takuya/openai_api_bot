9220baebd316d361c086ed559b19399141c0df6c
mori-takuya <58283134+mori-takuya@users.noreply.github.com>
2023-10-29 14:48:01 +0900

    Update home.py

--- src/home.py
+++ src/home.py
@@ -2,19 +2,15 @@ import streamlit as st
 import streamlit_authenticator as stauth
 
 # ユーザ情報。引数
-#names = st.secrets.login.names  # 
-#usernames = st.secrets.login.usernames  # 入力フォームに入力された値と合致するか確認される
-#passwords = st.secrets.login.passwords  # 入力フォームに入力された値と合致するか確認される
-names = ['John Smith', 'Rebecca Briggs']  # 
-usernames = ['jsmith', 'rbriggs']  # 入力フォームに入力された値と合致するか確認される
-passwords = ['123', '456']  # 入力フォームに入力された値と合致するか確認される
-
+names = st.secrets.login.names  # 
+usernames = st.secrets.login.usernames  # 入力フォームに入力された値と合致するか確認される
+passwords = st.secrets.login.passwords  # 入力フォームに入力された値と合致するか確認される
 
 # パスワードをハッシュ化。 リスト等、イテラブルなオブジェクトである必要がある
 hashed_passwords = stauth.Hasher(passwords).generate()
 
 # cookie_expiry_daysでクッキーの有効期限を設定可能。認証情報の保持期間を設定でき値を0とするとアクセス毎に認証を要求する
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,'some_cookie_name', 'some_signature_key', cookie_expiry_days='30')
 
 # ログインメソッドで入力フォームを配置
 name, authentication_status, username = authenticator.login('Login', 'main')
