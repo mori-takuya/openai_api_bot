63b7433df047149e6cccab698efb1ef10b4fd1e1
mori-takuya <58283134+mori-takuya@users.noreply.github.com>
2023-10-29 18:25:34 +0900

    Update home.py

--- src/home.py
+++ src/home.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
+import urllib2
 
 url = 'https://raw.githubusercontent.com/mori-takuya/openai_api_bot/main/src/config.yaml'
-with open(url) as file:
+with urllib2.urlopen(url) as file:
     config = yaml.load(file, Loader=yaml.SafeLoader)
 	
 authenticator = stauth.Authenticate(
