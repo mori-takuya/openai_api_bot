770fcb4f1823bf8fadc66b95139f10e16cdd3c7c
mori-takuya <58283134+mori-takuya@users.noreply.github.com>
2023-10-29 17:48:13 +0900

    Update home.py

--- src/home.py
+++ src/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 
-with open('src/config.yaml') as file:
+with open('config.yaml') as file:
     config = yaml.load(file, Loader=yaml.SafeLoader)
 
 authenticator = stauth.Authenticate(
