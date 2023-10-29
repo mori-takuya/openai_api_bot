e4fade778759d95fa2debadc709393d22e5bbb87
mori-takuya <58283134+mori-takuya@users.noreply.github.com>
2023-10-29 13:59:50 +0900

    Add files via upload

--- src/home.py
+++ src/home.py
@@ -1,3 +1,28 @@
 import streamlit as st
-st.title("Test")
-st.write("page_test")
+import streamlit_authenticator as stauth
+
+# ユーザ情報。引数
+names = st.secrets.login.names  # 
+usernames = st.secrets.login.usernames  # 入力フォームに入力された値と合致するか確認される
+passwords = st.secrets.login.passwords  # 入力フォームに入力された値と合致するか確認される
+
+# パスワードをハッシュ化。 リスト等、イテラブルなオブジェクトである必要がある
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+# cookie_expiry_daysでクッキーの有効期限を設定可能。認証情報の保持期間を設定でき値を0とするとアクセス毎に認証を要求する
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
+
+# ログインメソッドで入力フォームを配置
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+# 返り値、authenticaton_statusの状態で処理を場合分け
+if authentication_status:
+    # logoutメソッドでaurhenciationの値をNoneにする
+    authenticator.logout('Logout', 'main')
+    st.write('Welcome *%s*' % (name))
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
