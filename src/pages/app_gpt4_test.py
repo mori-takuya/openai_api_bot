8b46a586116058180ca3f371a090224768a151fc
mm <58283134+mori-takuya@users.noreply.github.com>
2023-11-09 17:32:22 +0900

    Add files via upload

--- src/pages/app_gpt4_test.py
+++ src/pages/app_gpt4_test.py
@@ -27,25 +27,25 @@ IT技術以外のことは絶対に答えないでください。
 """
 
 # st.session_stateを使いメッセージのやりとりを保存
-if "messages_test" not in st.session_state:
-    st.session_state["messages_test"] = [
+if "messages_gpt4_test" not in st.session_state:
+    st.session_state["messages_gpt4_test"] = [
         {"role": "system", "content": system_prompt}
         ]
 
 # チャットボットとやりとりする関数
 def communicate():
-    messages_test = st.session_state["messages_test"]
+    messages_gpt4_test = st.session_state["messages_gpt4_test"]
 
     user_message_test = {"role": "user", "content": st.session_state["user_input"]}
-    messages_test.append(user_message_test)
+    messages_gpt4_test.append(user_message_test)
 
     response = openai.ChatCompletion.create(
         model="gpt-4-1106-preview",
-        messages=messages_test
+        messages=messages_gpt4_test
     )  
 
     bot_message_test = response["choices"][0]["message"]
-    messages_test.append(bot_message_test)
+    messages_gpt4_test.append(bot_message_test)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
 
@@ -55,10 +55,10 @@ st.title("ITアシスタントbot")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
-if st.session_state["messages_test"]:
-    messages_test = st.session_state["messages_test"]
+if st.session_state["messages_gpt4_test"]:
+    messages_gpt4_test = st.session_state["messages_gpt4_test"]
 
-    for message_test in reversed(messages_test[1:]):  # 直近のメッセージを上に
+    for message_test in reversed(messages_gpt4_test[1:]):  # 直近のメッセージを上に
         speaker = "自分"
         if message_test["role"]=="assistant":
             speaker="AI"
