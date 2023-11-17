e20de8ec2c1a2b37c52e170ba2255f437bb3f573
mm <58283134+mori-takuya@users.noreply.github.com>
2023-11-18 02:56:36 +0900

    Update app.py

--- src/pages/app.py
+++ src/pages/app.py
@@ -31,7 +31,7 @@ def communicate():
         messages=messages
     )  
 
-    bot_message = response.choices[0].message.content
+    bot_message = response.choices[0].message
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
