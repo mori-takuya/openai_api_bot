aee27158d01a4ed814038ae2f53e08f2dc34080a
mm <58283134+mori-takuya@users.noreply.github.com>
2023-11-17 16:59:16 +0900

    Update app.py

--- src/pages/app.py
+++ src/pages/app.py
@@ -29,10 +29,10 @@ def communicate():
 
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=messages,
+        messages=messages
     )  
 
-    bot_message = response.choices[0].messages
+    bot_message = response.choices[0].message.content
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
