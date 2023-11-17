4ca14479f8439c02a86ad000700e0f35b8403799
mm <58283134+mori-takuya@users.noreply.github.com>
2023-11-18 00:33:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,24 @@ client = OpenAI(
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたはプログラマのアシスタントです"}
-        ]
+    ]
 
 # チャットボットとやりとりする関数
 def communicate():
     messages = st.session_state["messages"]
 
-    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    user_message = {"role": "user", "content": st.session_state.user_input}
     messages.append(user_message)
 
-    response = client.chat.completions.create(
+    response = client.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
-    )  
+    )
 
-    bot_message = response.choices[0].message.content
-    messages.append(bot_message)
+    bot_message = response.choices[0].message["content"]
+    messages.append({"role": "assistant", "content": bot_message})
 
-    st.session_state["user_input"] = ""  # 入力欄を消去
-
+    st.session_state.user_input = ""  # 入力欄を消去
 
 # ユーザーインターフェイスの構築
 st.title("AI Assistant")
@@ -41,9 +40,6 @@ if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
-        speaker = "自分"
-        if message.role=="assistant":
-            speaker="AI"
-
-        st.write(speaker + ": " + message.content)
+        speaker = "自分" if message["role"] == "user" else "AI"
+        st.write(f"{speaker}: {message['content']}")
         st.write("-----------------------------------------------------------------")
