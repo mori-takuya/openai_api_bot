676146653141582964face03e6f5bd510aec0621
mm <58283134+mori-takuya@users.noreply.github.com>
2023-11-18 18:12:35 +0900

    Update app.py

--- src/pages/app.py
+++ src/pages/app.py
@@ -48,7 +48,7 @@ if st.session_state["messages"]:
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
         speaker = "自分"
-        if message.roles=="assistant" :
+        if message.role=="assistant" :
             speaker="AI"
         st.write(speaker + ": " + message.content)
         st.write("-----------------------------------------------------------------")
